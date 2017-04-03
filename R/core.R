a81d690d8ab9389fef8ca7122447accb35bb0018
pierzgal <pierzgal@gmail.com>
2017-04-03 03:35:41 +0200

    Add files via upload

--- R/core.R
+++ R/core.R
@@ -935,7 +935,7 @@ Disp2 <- function(seed = 1000,
 
   plot_disp3 <-
     ggplot2::ggplot(data = dplyr::filter(lghi_all, method %in% c("DH", "SL", "MSL", "H") )) + ggplot2::geom_point(ggplot2::aes(
-      x = as.factor(DM),
+      x = DM,
       y = GHI,
       color = factor(method)
     ), size = 0.8) + viridis::scale_color_viridis(option = "D", discrete = TRUE) +  ggplot2::xlab("DM") + ggplot2::ylab("GHI") + ggplot2::labs(color = "Method") + ggplot2::geom_hline(yintercept = c(0.1), size = 0.35, linetype = "longdash", colour = "blue") + ggplot2::theme_classic() + ggplot2::theme(
